07dd664c47d1eb1ad33b81fbe119b044237a7444
Danie Jianah Sonizara <ds294702@creusot.u-bourgogne.fr>
2016-09-21 19:23:22 +0200

    29/09/16

---
+++ calculateArea.m
@@ -0,0 +1,3 @@
+function area = calculateArea(x,y)
+% Function to calculate an area given two lengths (x,y)
+area = x*y;
---
+++ ex1.m
@@ -0,0 +1,2 @@
+syms n x %declare les variableS
+e= symsum (x^n/factorial(n), n, 0, 20 )
